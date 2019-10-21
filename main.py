573ee09a247e38016f3edcdc130df96c2d548357
Alan Tan <alanwuha91@gmail.com>
2019-10-21 23:51:15 +0800

    small fix

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         print()
 
     time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapse // 60, time_elapsed % 60))
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
 
     # load best model weights
