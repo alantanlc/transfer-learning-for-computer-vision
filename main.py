fe48bf51f60f1e34a73d826d54f4a22fd0ef3598
Alan Tan <alanwuha91@gmail.com>
2019-10-23 22:50:34 +0800

    fixed indentation

--- main.py
+++ main.py
@@ -82,15 +82,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
 
-            print()
+        print()
 
-        time_elapsed = time.time() - since
-        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-        print('Best val Acc: {:4f}'.format(best_acc))
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
 
-        # load best model weights
-        model.load_state_dict(best_model_wts)
-        return model
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 # Generic function to display predictions for a few images
 def visualize_model(model, num_images=6):
