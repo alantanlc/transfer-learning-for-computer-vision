1e2eebea444e1673fa988883b9d8b1ba0c21ddba
Alan Tan <alanwuha91@gmail.com>
2019-10-26 22:01:07 +0800

    removed dataparallel and replace hamming with running corrects

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -9,8 +10,6 @@ import matplotlib.pyplot as plt
 import time
 import copy
 from CheXpert import *
-from tqdm import tqdm
-from sklearn.metrics import hamming_loss
 
 plt.ion()   # interactive mode
 
@@ -19,7 +18,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
-    best_hamming_score = 0.0
+    best_acc = 0.0
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -33,8 +32,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 model.eval()    # Set model to evaluate mode
 
             running_loss = 0.0
-            running_hamming_loss = 0
-            running_hamming_score = 0
+            running_corrects = 0
 
             # Iterate over data
             for i_batch, sample_batched in enumerate(dataloaders[phase]):
@@ -55,8 +53,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                     preds = (probs > 0.5).long()
                     y_pred = preds.cpu().numpy()
                     y_true = labels.cpu().numpy()
-                    ham_loss = hamming_loss(y_true, y_pred)
-                    ham_score = (y_true == y_pred).mean()
 
                     # backward + optimize only if in training phase
                     if phase == 'train':
@@ -65,26 +61,24 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
-                running_hamming_loss = (running_hamming_loss + ham_loss) / 2
-                running_hamming_score = (running_hamming_score + ham_score) / 2
+                running_corrects += np.sum(y_true == y_pred)
             if phase == 'train':
                 scheduler.step()
 
             epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_hamming_loss = running_hamming_loss
-            epoch_hamming_score = running_hamming_score
+            epoch_acc = float(running_corrects) / (dataset_sizes[phase] * 14)
 
-            print('{} Loss: {:.4f} Hamming Loss: {:.4f} Hamming Score: {:.4f}'.format(phase, epoch_loss, epoch_hamming_loss, epoch_hamming_score))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
-            if phase == 'valid' and epoch_hamming_score > best_hamming_score:
-                best_hamming_score = epoch_hamming_score
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
         print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val Hamming Score: {:4f}'.format(best_hamming_score))
+    print('Best val Acc: {:4f}'.format(best_acc))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
@@ -120,7 +114,7 @@ csv_dir = '/home/alanwuha/Documents/Projects/ce7454-grp17/data/CheXpert-v1.0-sma
 
 image_datasets = {x: CheXpertDataset(csv_file=os.path.join(csv_dir, x + '.csv'), root_dir=root_dir, transform=data_transforms[x]) for x in ['train', 'valid']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
-dataloaders = {x: DataLoader(image_datasets[x], batch_size=16, shuffle=True, num_workers=4) for x in ['train', 'valid']}
+dataloaders = {x: DataLoader(image_datasets[x], batch_size=5, shuffle=True, num_workers=4) for x in ['train', 'valid']}
 print('dataset_sizes:', dataset_sizes)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -133,27 +127,12 @@ model_conv = models.resnet18(pretrained=True)
 # for param in model_conv.parameters():
 #     param.requires_grad = False
 
-# Use multiple GPUs
-if torch.cuda.device_count() > 1:
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
+# Parameters of newly constructed modules have required_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+model_conv.fc = nn.Linear(num_ftrs, 14)
 
-    # Wrap model using nn.DataParallel
-    model_conv = nn.DataParallel(model_conv)
-
-    # Parameters of newly constructed modules have required_grad=True by default
-    num_ftrs = model_conv.module.fc.in_features
-    model_conv.module.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    model_conv.module.fc = nn.Linear(num_ftrs, 14)
-
-    optimizer_conv = optim.SGD(model_conv.module.parameters(), lr=0.001, momentum=0.9)
-    print(model_conv.module.parameters())
-else:
-    # Parameters of newly constructed modules have required_grad=True by default
-    num_ftrs = model_conv.fc.in_features
-    model_conv.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    model_conv.fc = nn.Linear(num_ftrs, 14)
-
-    optimizer_conv = optim.SGD(model_conv.parameters(), lr=0.001, momentum=0.9)
+optimizer_conv = optim.Adam(model_conv.parameters(), lr=0.001)
 
 # Send model to device
 model_conv = model_conv.to(device)
